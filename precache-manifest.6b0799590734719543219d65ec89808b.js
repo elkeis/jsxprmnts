b572569bac90dd28769deb8b095bdb544127582f
Kiryl Y <kiryll.e@gmail.com>
2019-04-16 01:07:53 +0200

    Updates

---
+++ precache-manifest.6b0799590734719543219d65ec89808b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8eda5152131f172a1606",
+    "url": "/jsxprmnts/static/css/main.8d3a641d.chunk.css"
+  },
+  {
+    "revision": "8eda5152131f172a1606",
+    "url": "/jsxprmnts/static/js/main.d9839c1f.chunk.js"
+  },
+  {
+    "revision": "b0bc529b68e9e810d609",
+    "url": "/jsxprmnts/static/js/runtime~main.db5d2d39.js"
+  },
+  {
+    "revision": "2784206719b4d7a3574c",
+    "url": "/jsxprmnts/static/js/2.096a3b10.chunk.js"
+  },
+  {
+    "revision": "e232eb8484c79d5974e54b7c690fc70b",
+    "url": "/jsxprmnts/static/media/rick-and-morty.e232eb84.png"
+  },
+  {
+    "revision": "c2a131c5aa8bd2e77eccf9998478b944",
+    "url": "/jsxprmnts/index.html"
+  }
+];
