5655b4828dafa7b1cb459ee03ccf13ad3601cdf1
Kiryl Y <kiryll.e@gmail.com>
2019-04-16 22:49:35 +0200

    fix typo

--- src/pages/ImageSlice/sliceCountAlert.js
+++ src/pages/ImageSlice/sliceCountAlert.js
@@ -1,7 +1,7 @@
 export default function throwSliceCountAlert(imageSlicePropsObj) {
     if (imageSlicePropsObj.sliceCount >= 400) {
         debugger;
-        if (!window.confirm("Next increasing slice count more could crash your machine! Continue?")) {
+        if (!window.confirm("Increasing slice count could crash your machine! Continue?")) {
             return false;
         }
     }
